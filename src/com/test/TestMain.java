d3f8c4a4bcdcdc74e6c8703afbe6d8f8f504cd40
jsJasonwu <wufeng135@163.com>
2017-05-05 13:48:04 +0800

    first commit

---
+++ src/com/test/TestMain.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestMain {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader strin=new BufferedReader(new InputStreamReader(System.in));  
+        System.out.print("ÇëÊäÈëÒ»¸ö×Ö·û´®£º");  
+		System.out.println(strin.readLine());
+	}
+}
